aaf5e4aec616c90998df97a41f2227b061997f30
EricZhangXu <eric.zhang@3plearning.com>
2021-11-16 09:10:22 +1100

    Added flask_restx and swagger documents

--- app.py
+++ app.py
@@ -1,45 +1,70 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+from flask_restx import Api, Resource, fields
 import sympy
 
 
 app = Flask(__name__)
+api = Api(app, version='0.1', title='Questicon Validation API',
+          description='Questicon Validation API')
+ns = api.namespace("sympy", description="Sympy Validaiton API")
+
+factor_request = api.model(
+    "factor_fields",
+    {
+        "student_response": fields.String(required=True, description="student answer")
+    }
+)
+
+compare_request = api.model(
+    "compare_fields",
+    {
+        "student_response": fields.String(required=True, description="student answer"),
+        "valid_response": fields.String(required=True, description="valid answer")
+    }
+)
+
+result = api.model(
+    'result_model',
+    {
+        'result': fields.String,
+    }
+)
 
 
-@app.route('/')
-def index():
-    return jsonify({'name': 'math',
-                    'email': 'math@outlook.com'})
+@ns.route('/factor',endpoint='factor-endpoint')
+class FactorClass(Resource):
+    @api.doc(id='factor', body=factor_request)
+    @api.response(200, 'Success', result)
+    def post(self):
+        request_data = api.payload
+        student_answer = None
+
+        if request_data:
+            if 'student_response' in request_data:
+                student_response = request_data['student_response']
+                student_answer = sympy.factor(student_response)
+                print(student_answer)
+
+        return jsonify({'result': '{}'.format(student_answer)})
 
 
-@app.route('/factor', methods=['POST'])
-def factor():
-    request_data = request.get_json()
-    student_answer = None
+@ns.route('/compare',endpoint='compare-endpoint')
+class CompareClass(Resource):
+    @api.doc(id='compare',body=compare_request)
+    @api.response(200, 'Success', result)
+    def post(self):
+        request_data = api.payload
+        student_answer = None
+        valid_answer = None
 
-    if request_data:
-        if 'student_response' in request_data:
-            student_response = request_data['student_response']
-            student_answer = sympy.factor(student_response)
-            print(student_answer)
+        if request_data:
+            if 'valid_response' in request_data:
+                student_answer = sympy.simplify(request_data['valid_response'])
 
-    return jsonify({'result': '{}'.format(student_answer)})
+            if 'student_response' in request_data:
+                valid_answer = sympy.simplify(request_data['student_response'])
 
-
-@app.route('/compare', methods=['POST'])
-def compare():
-    request_data = request.get_json()
-
-    student_answer = None
-    valid_answer = None
-
-    if request_data:
-        if 'valid_response' in request_data:
-            student_answer = sympy.simplify(request_data['valid_response'])
-
-        if 'student_response' in request_data:
-            valid_answer = sympy.simplify(request_data['student_response'])
-
-    return jsonify({'result': '{}'.format(student_answer == valid_answer)})
+        return jsonify({'result': '{}'.format(student_answer == valid_answer)})
 
 
 if __name__ == '__main__':
